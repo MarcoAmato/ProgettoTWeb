7b6ab81a3512bac408822af7bc8ba19e05880ae9
Marco Amato <amatomarco111@gmail.com>
2021-01-12 17:46:39 +0100

    Signup works, next redirect feedback

--- php/common.php
+++ php/common.php
@@ -1,7 +1,7 @@
 <?php
 
 function dbconnect() {
-	$db = new PDO('mysql:dbname=imdb;host=localhost:3306', "root", "");
+	$db = new PDO('mysql:dbname=gamehub;host=localhost:3306', "root", "");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $db;
 }
@@ -9,6 +9,11 @@ function dbconnect() {
 function isDate($dateString){
 	return DateTime::createFromFormat('Y-m-d', $dateString) !== false;
 	//allora l'oggetto viene creato correttamente e di conseguenza la stringa è nel formato giusto
+}
+
+function redirectWithError($path, $error){
+	header("Location :$path?error=$error");
+	exit;
 }
 
 ?>
--- php/sign-up.php
+++ php/sign-up.php
@@ -1,4 +1,6 @@
 <?php
+    $PATH_TO_REDIRECT = "../html/complete/sign-up.shtml"
+
     if(!isset($_SERVER["REQUEST_METHOD"]) || $_SERVER["REQUEST_METHOD"] != "GET"){
         header("HTTP/1.1 400 Invalid Request");
         die("ERROR 400: Invalid request - This service accepts only GET requests.");
@@ -9,33 +11,40 @@
     try {
         $db = dbconnect();
     } catch (PDOException $e) {
-        echo 'error pdo failed';
-        exit;
+        redirectWithError($PATH_TO_REDIRECT, "server down");
     }
 
+    //clan input
     if(!isset($_GET['email']) || !isset($_GET['password']) || !isset($_GET['nascita']) || !isset($_GET['genere'])){
-        echo("error variables not set");
-        exit;
+        redirectWithError($PATH_TO_REDIRECT,"variables not set");
 	}
 	
 	if(!isDate($_GET['nascita'])){
-		echo("error date format is not valid");
-        exit;
+        redirectWithError($PATH_TO_REDIRECT,"wrong date format");
 	}
 
 	if($_GET['genere'] !== "m"
 		&& $_GET['genere'] !== "f"){
-			echo("error there are only 2 genders");
-			exit;
+            redirectWithError($PATH_TO_REDIRECT,"wrong gender");
 	}
 
+    //compose query
     $email = $db->quote($_GET['email']);
-    $hashed_password = password_hash($db->quote($_GET['password']));
+    $hashed_password = hash('md5', $db->quote($_GET['password']));
 	$nascita = $_GET['nascita'];
 	$genere = $_GET['genere'];
 
 	$insert_query ="
 		INSERT INTO utenti (`email`,`password`,`data_di_nascita`,`genere`)
-		VALUES (`$email`,`$hashed_password`,`$nascita`,`$genere`);
-	"
+		VALUES ($email,'$hashed_password','$nascita','$genere');
+    ";
+    $result = $db->query($insert_query);
+    //send it to database
+    if(!$result){
+        //result is null = error in processing query
+        redirectWithError($PATH_TO_REDIRECT, "query failed");
+    }else{
+        header("Location: $PATH_TO_REDIRECT?success=true");
+        exit;
+    }
 ?>
