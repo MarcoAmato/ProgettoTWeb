e5d6ddaaae6b0337d2b167c6f7439579924196cc
Marco Amato <amatomarco111@gmail.com>
2021-01-24 15:45:52 +0100

    Search form client side validation done

--- js/search.js
+++ js/search.js
@@ -2,49 +2,51 @@ const searchErrorMap = new Map();
 searchErrorMap.set("server_down", "Il server non è raggiungibile, riprovare più tardi");
 searchErrorMap.set("query_failed", "Non è possibile visualizzare gli annunci, riprovare più tardi");
 
-$(function(){
+$(function () {
     let nome = getURLParameter("nome");
     let piattaforma = getURLParameter("piattaforma");
 
-    if(!nome || !piattaforma){
+    if (!nome || !piattaforma) {
         showSearch("variables_not_set");
         return;
     }
 
-    if(piattaforma === "all"){
+    console.log(nome);
+    console.log(piattaforma);
+
+    if (piattaforma === "all") {
         $("#advertisements").append('<h1> ' + nome + ' - tutte le piattaforme </h1>');
-    }else{
+    } else {
         $("#advertisements").append('<h1> ' + nome + ' - ' + piattaforma + ' </h1>');
     }
 
     $.post({
-       url: "../../php/search.php",
-       datatype: "json",
-       data: {
-           'nome': nome,
-           'piattaforma': piattaforma
-       },
-       success: showSearch,
-       error: function(){
-           showSearch("server_unreachable");
-       }
+        url: "../../php/search.php",
+        datatype: "json",
+        data: {
+            'nome': nome,
+            'piattaforma': piattaforma
+        },
+        success: showSearch,
+        error: function () {
+            showSearch("server_unreachable");
+        }
     });
 });
 
-function showSearch(data){
+function showSearch(data) {
     let jsonAnnunci = null;
-    try{
+    try {
         jsonAnnunci = JSON.parse(data);
-    }catch(e){
-        console.log("error: "+data);
+    } catch (e) {
+        console.log("error: " + data);
         let searchErrorElement = $("#search-error");
         let defaultErrorText = "Si è verificato un errore, riprova più tardi";
         fillError(searchErrorElement, searchErrorMap, data, defaultErrorText);
         return;
     }
-    
-    console.log("jsonAnnunci: "+jsonAnnunci);
-    for(annuncio of jsonAnnunci){ /**
+
+    for (annuncio of jsonAnnunci) { /**
      * <div class="advert">
             <div class="advert-image">
                 <img src="../../img/god_of_war.jpeg" alt="">
@@ -57,18 +59,18 @@ function showSearch(data){
         </div>
      */
         let divAdvert = '<div class="advert">';
-            if(annuncio.path_immagine !== null){
-                let imgAdvert = '<img src="../../img/advert-img/'+ annuncio.path_immagine + '" alt="'+ annuncio.titolo +'">';
-                
-                divAdvert += "<div class='advert-image'>";
-                    divAdvert += imgAdvert;
-                divAdvert += "</div>";
-            }
-            divAdvert += "<div class='advert-text'>";
-                divAdvert += '<h2>' + annuncio.titolo + '</h2>';
-                divAdvert += '<p>' + annuncio.testo + '</p>';
+        if (annuncio.path_immagine !== null) {
+            let imgAdvert = '<img src="../../img/advert-img/' + annuncio.path_immagine + '" alt="' + annuncio.titolo + '">';
+
+            divAdvert += "<div class='advert-image'>";
+            divAdvert += imgAdvert;
             divAdvert += "</div>";
-            divAdvert += '<img src="../../img/icons/empty_heart.png" alt="" class="heart">';
+        }
+        divAdvert += "<div class='advert-text'>";
+        divAdvert += '<h2>' + annuncio.titolo + '</h2>';
+        divAdvert += '<p>' + annuncio.testo + '</p>';
+        divAdvert += "</div>";
+        divAdvert += '<img src="../../img/icons/empty_heart.png" alt="" class="heart">';
         divAdvert += '</div>';
 
         $("#advertisements").append(divAdvert);
