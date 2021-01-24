c9f731facfcc85f06b24206d1258ba38e738e318
Marco Amato <amatomarco111@gmail.com>
2021-01-24 14:54:40 +0100

    Image in new-advert is removed in case of errors

--- php/new-advert.php
+++ php/new-advert.php
@@ -1,6 +1,7 @@
 <?php
     $PATH_TO_INDEX = "../html/complete/index.shtml";
     $PATH_TO_NEW_ADVERT = "../html/complete/new-advert.shtml";
+    $PATH_TO_IMG_FOLDER = "../img/advert-img";
 
     if(!isset($_SERVER["REQUEST_METHOD"]) || $_SERVER["REQUEST_METHOD"]!="POST"){
         header("HTTP/1.1 400 Invalid Request");
@@ -22,23 +23,6 @@
         redirectWithError($PATH_TO_NEW_ADVERT, "server_down");
     }
 
-    //image check
-    $imagePath = null;
-    if(isset($_FILES["immagine"]["tmp_name"]) && is_uploaded_file($_FILES['immagine']['tmp_name'])){
-        //Image is inserted, we need to check it and save it
-        $imageSaveResult = checkAndSaveImg($_FILES['immagine'], "../img/advert-img");
-        if($imageSaveResult['error'] != null){
-            //Error in saving image occured, we give feedback to caller
-            redirectWithError($PATH_TO_NEW_ADVERT, $imageSaveResult['error']);
-        }else if(($imageSaveResult['success'])!=null){
-            //Image was saved correctly, we save the name in the database
-            $imagePath = "'".$imageSaveResult['success']."'";
-        }else{
-            //If the computation arrives here something strange happened
-            redirectWithError($PATH_TO_NEW_ADVERT, "unexpected_image_error");
-        }
-    }
-
     //check if input is inserted
     if(!isset($_POST['piattaforma']) || !isset($_POST['titolo'])){
         redirectWithError($PATH_TO_NEW_ADVERT,"variables_not_set");
@@ -53,38 +37,56 @@
     }
 
     //Check piattaforma
-        $queryPiattaforme = "
-        SELECT nome
-        FROM piattaforme
-        ORDER BY nome
-        ";
+    $queryPiattaforme = "
+    SELECT nome
+    FROM piattaforme
+    ORDER BY nome
+    ";
 
-        try{
-            $resultQueryPiattaforme = $db->query($queryPiattaforme);
-        }catch(Exception $e){
-            redirectWithError($PATH_TO_NEW_ADVERT, 'query_failed');
-        }
+    try{
+        $resultQueryPiattaforme = $db->query($queryPiattaforme);
+    }catch(Exception $e){
+        redirectWithError($PATH_TO_NEW_ADVERT, 'query_failed');
+    }
 
-        if(!$resultQueryPiattaforme){
-            redirectWithError($PATH_TO_NEW_ADVERT, 'query_failed');
-        }
-        
-        $piattaformeDisponibili = $resultQueryPiattaforme->fetchAll(PDO::FETCH_COLUMN);
-
-        $piattaformaValid = false;
-        $i = 0;
-        while(!$piattaformaValid && $i < count($piattaformeDisponibili)){
-            $p = $piattaformeDisponibili[$i];
-            if($db->quote($p) == $piattaforma){
-                $piattaformaValid = true;
-            }
-            $i++;
-        }
-
-        if(!$piattaformaValid){
-            redirectWithError($PATH_TO_NEW_ADVERT, "piattaforma_unexpected");
-        }
+    if(!$resultQueryPiattaforme){
+        redirectWithError($PATH_TO_NEW_ADVERT, 'query_failed');
+    }
     
+    $piattaformeDisponibili = $resultQueryPiattaforme->fetchAll(PDO::FETCH_COLUMN);
+
+    $piattaformaValid = false;
+    $i = 0;
+    while(!$piattaformaValid && $i < count($piattaformeDisponibili)){
+        $p = $piattaformeDisponibili[$i];
+        if($db->quote($p) == $piattaforma){
+            $piattaformaValid = true;
+        }
+        $i++;
+    }
+
+    if(!$piattaformaValid){
+        redirectWithError($PATH_TO_NEW_ADVERT, "piattaforma_unexpected");
+    }
+
+    //image check
+    $imagePath = null;
+    if(isset($_FILES["immagine"]["tmp_name"]) && is_uploaded_file($_FILES['immagine']['tmp_name'])){
+        //Image is inserted, we need to check it and save it
+        $imageSaveResult = checkAndSaveImg($_FILES['immagine'], $PATH_TO_IMG_FOLDER);
+        if($imageSaveResult['error'] != null){
+            //Error in saving image occured, we give feedback to caller
+            redirectWithError($PATH_TO_NEW_ADVERT, $imageSaveResult['error']);
+        }else if(($imageSaveResult['success'])!=null){
+            //Image was saved correctly, we save the name in the database
+            $imagePath = "'".$imageSaveResult['success']."'";
+        }else{
+            //If the computation arrives here something strange happened
+            redirectWithError($PATH_TO_NEW_ADVERT, "unexpected_image_error");
+        }
+    }
+
+   
     $queryNewAdvert = 
     "INSERT INTO `annunci` VALUES
     (null, '$email', $piattaforma, $titolo, $testo, $imagePath);
@@ -93,8 +95,7 @@
     try{
         $result = $db->query($queryNewAdvert);
     }catch(Exception $e){
-        print_r($db->errorInfo());
-        exit;
+        unlink($PATH_TO_IMG_FOLDER.'/'.$imageSaveResult['success']);
         redirectWithError($PATH_TO_NEW_ADVERT,"query_failed");
     }
 
